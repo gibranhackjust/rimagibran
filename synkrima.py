2f1de7eb75ea351b9cacc765ff1ff539fd3f7706
gibranhackjust <78136331+gibranhackjust@users.noreply.github.com>
2021-01-28 16:40:33 +0800

    Add files via upload

---
+++ synkrima.py
@@ -0,0 +1,58 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 3
+echo
+echo $green"No Wa : 085238506497"
+echo $green"Email : gibranfancy@gmail.com"
+echo $green"admin : gibran"
+echo $green"autor : M SYAESAR GIBRAN"
+echo
+echo $cyan"PILIH MENU TOOL : "
+echo
+echo $red"============================"
+echo $purple"[01].Hack Facebook Crack"
+echo $red"============================"
+echo $purple"[02].Hack Instagram(ROOT)"
+echo $red"============================"
+echo $purple"[03].Hack Akun Game online"
+echo $red"============================"
+echo $purple"[04].Hack Diamond GAME"
+echo $red"============================"
+echo $purple"[05].Hack NASA (ROOT)"
+echo $red"============================"
+echo $purple"[06].Root HandPhone"
+echo $red"============================"
+echo $purple"[07].Hack WEBSITE"
+echo $red"============================"
+echo $purple"[08].Hack Satelit"
+echo $red"============================"
+echo $purple"[09].Hack CCTV"
+echo $red"============================"
+echo $purple"[10].Hack Listrik Rumah"
+echo $red"============================"
+echo $purple"[11].Hack Kuota"
+echo $red"============================"
+echo $purple"[12].Hack Pulsa"
+echo $red"============================"
+echo $purple"[13].Hack TV rumah"
+echo $red"============================"
+echo $purple"[14].Hack Running Text"
+echo $red"============================"
+echo $purple"[15].Hack WIFI tetangga"
+echo $red"============================"
+echo $purple"[16].Hack Komputer"
+echo $red"============================"
+echo $purple"[17].Spam SMS"
+echo $red"============================"
+echo $purple"[18].Kirim Virus HP"
+echo $red"============================"
+echo $purple"[19].Hack Tiket Pesawat"
+echo $red"============================"
+echo $purple"[20].Keluar Tools"
+echo $red"============================"
